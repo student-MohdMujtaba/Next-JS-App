f25a81594daefe621f8369dfe84aa33c22bcb5b1
Mohd Mujtaba <mohdmujtaba136@gmail.com>
2025-08-09 12:43:48 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Componenets/Navbar'
+
+const page = () => {
+  return (
+    <div className='flex flex-col  min-h-screen bg-[url("https://mern-authentication-amber.vercel.app/bg_img.png")] bg-cover bg-center '>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default page
