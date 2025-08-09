be1353335aae5bb60e0bf7a83fef376aeb92367d
Mohd Mujtaba <mohdmujtaba136@gmail.com>
2025-08-09 13:10:19 +0530

    update first commit

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ const page = () => {
   return (
     <div className='flex flex-col  min-h-screen bg-[url("https://mern-authentication-amber.vercel.app/bg_img.png")] bg-cover bg-center '>
         <Navbar/>
+      <div></div>
     </div>
   )
 }
